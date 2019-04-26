d7d7248d7e3b3c4d031a075e0fb356ce957d8800
= <=>
2019-04-26 23:33:51 +0800

    <upd

--- app/src/main/java/shlm/lmcs/com/lazycat/LazyShopAct/SearchAct.java
+++ app/src/main/java/shlm/lmcs/com/lazycat/LazyShopAct/SearchAct.java
@@ -1,1 +1,206 @@
-package shlm.lmcs.com.lazycat.LazyShopAct;import android.annotation.SuppressLint;import android.os.Bundle;import android.text.Editable;import android.text.TextUtils;import android.text.TextWatcher;import android.view.LayoutInflater;import android.view.View;import android.view.ViewGroup;import android.widget.BaseAdapter;import android.widget.EditText;import android.widget.LinearLayout;import android.widget.ListView;import android.widget.TextView;import java.util.ArrayList;import shlm.lmcs.com.lazycat.LazyCatProgramUnt.CompanyAct.LazyCatAct;import shlm.lmcs.com.lazycat.LazyCatProgramUnt.Tools;import shlm.lmcs.com.lazycat.LazyCatProgramUnt.Views.AutoLineLayout;import shlm.lmcs.com.lazycat.R;/** * æœç´¢ç•Œé¢ */public class SearchAct extends LazyCatAct {    private AutoLineLayout everyoneSearch;    private AutoLineLayout searchLog;    private EditText input;    private ListView SearchlogListview;    private TextView btn_search;    @SuppressLint({"NewApi", "ResourceType"})    @Override    protected void onCreate(Bundle savedInstanceState) {        setContentView(R.layout.activity_search);        setTransparentBar();        super.onCreate(savedInstanceState);        input = findViewById(R.id.assembly_head_input);        input.setBackground(Tools.CreateDrawable(3, getResources().getString(R.color.ThemeColor),                "#ffffff", 10));        btn_search = findViewById(R.id.assembly_head_searchTitle);        SearchlogListview = findViewById(R.id.activity_search_listview);        Tools.getFoucus(input);        init();    }    private void init() {        /**         * ç¼–è¾‘æ¡†çš„ç›‘å¬äº‹ä»¶         */        input.addTextChangedListener(new TextWatcher() {            @Override            public void beforeTextChanged(CharSequence s, int start, int count, int after) {            }            @Override            public void onTextChanged(CharSequence s, int start, int before, int count) {            }            @Override            public void afterTextChanged(Editable s) {                /**                 * æ£€æŸ¥æ˜¯å¦æ²¡æœ‰æ•°æ® å¦‚æœæ²¡æœ‰ å°±ä¸è¦è¿›è¡Œåˆ—è¡¨æ“ä½œ                 */                if (TextUtils.isEmpty(s.toString())) {                    /*ä¸ºç©º*/                    findViewById(R.id.activity_search_listview).setVisibility(View.GONE);                    findViewById(R.id.activity_search_RefreshScrollView).setVisibility(View                            .VISIBLE);                } else {                    /*ä¸ä¸ºç©º*/                    /*æ¨¡æ‹Ÿç½‘ç»œè®¿é—®æœ‰æ•°æ®*/                    ArrayList<String> text_list2 = new ArrayList<>();                    text_list2.add("æµæµªåœ°çƒ");                    text_list2.add("é€é¥æ•£äºº");                    text_list2.add("ä¸œå®«");                    text_list2.add("EXO");                    text_list2.add("è€ç•ªèŒ„");                    text_list2.add("å‡é¢éª‘å£«ZI-O");                    text_list2.add("ä¿¡èª“æ—¦æ—¦");                    SearchLogAdapter adapter = new SearchLogAdapter(text_list2);                    SearchlogListview.setAdapter(adapter);                    findViewById(R.id.activity_search_listview).setVisibility(View.VISIBLE);                    findViewById(R.id.activity_search_RefreshScrollView).setVisibility(View.GONE);                }                //æ‰§è¡ŒåŠ¨ç”»                if (TextUtils.isEmpty(s.toString())) {                    /*è¾“å…¥æ¡†çš„æ•°æ®ä¸ºç©º*/                    btn_search.clearAnimation();                    /*é€æ¸éšè—åŠ¨ç”»*/                    btn_search.startAnimation(Tools.clearOnalpha(1000, false));                    btn_search.setVisibility(View.GONE);                    /*listViewä¸æ˜¾ç¤º*/                } else if (btn_search.getVisibility() != View.VISIBLE) {                    //ä¸åœ¨æ˜¾ç¤º                    btn_search.clearAnimation();                    btn_search.setVisibility(View.VISIBLE);                    btn_search.clearAnimation();                    btn_search.startAnimation(Tools.createOnalpha(1000, false));                }            }        });        /**         * ç‚¹å‡»æœç´¢æ¡†çš„ç›‘å¬äº‹ä»¶         */        btn_search.setOnClickListener(new View.OnClickListener() {            @Override            public void onClick(View v) {            }        });        ArrayList<String> text_list = new ArrayList<>();        text_list.add("æµæµªåœ°çƒ");        text_list.add("é€é¥æ•£äºº");        text_list.add("ä¸œå®«");        text_list.add("EXO");        text_list.add("è€ç•ªèŒ„");        text_list.add("å‡é¢éª‘å£«ZI-O");        text_list.add("ä¿¡èª“æ—¦æ—¦");        ArrayList<String> text_list2 = new ArrayList<>();        text_list2.add("æµæµªåœ°çƒ");        text_list2.add("é€é¥æ•£äºº");        text_list2.add("ä¸œå®«");        text_list2.add("EXO");        text_list2.add("è€ç•ªèŒ„");        text_list2.add("å‡é¢éª‘å£«ZI-O");        text_list2.add("ä¿¡èª“æ—¦æ—¦");        ArrayList<LinearLayout> tv_list = Tools.handleToarraylist(getApplicationContext(),                text_list, 20, 10, 20, 10, "#e9e9e9", "#666666", 13);        ArrayList<LinearLayout> tv_list2 = Tools.handleToarraylist(getApplicationContext(),                text_list2, 20, 10, 20, 10, "#e9e9e9", "#666666", 13);        everyoneSearch = findViewById(R.id.activity_everyone_searchBody);/*å¤§å®¶éƒ½åœ¨æœ*/        searchLog = findViewById(R.id.activity_search_searchLogBody);/*ç”¨æˆ·çš„æœç´¢è®°å½•*/        for (int i = 0; i < tv_list.size(); i++) {            everyoneSearch.addView(tv_list.get(i));        }        for (int i = 0; i < tv_list2.size(); i++) {            searchLog.addView(tv_list2.get(i));        }    }    class SearchLogAdapter extends BaseAdapter {        private ArrayList<String> list;        public SearchLogAdapter(ArrayList<String> _list) {            this.list = _list;        }        @Override        public int getCount() {            return this.list.size();        }        @Override        public Object getItem(int position) {            return null;        }        @Override        public long getItemId(int position) {            return 0;        }        @SuppressLint("NewApi")        @Override        public View getView(int position, View convertView, ViewGroup parent) {            //assembly_searchact_showsearchitem_title            viewpage _viewpage = null;            if (convertView != null) {                _viewpage = (viewpage) convertView.getTag();            } else {                convertView = LayoutInflater.from(getApplicationContext()).inflate(R.layout                        .assembly_searchact_showsearchitem, null);                _viewpage = new viewpage();                _viewpage.title = convertView.findViewById(R.id                        .assembly_searchact_showsearchitem_title);                _viewpage._static = convertView.findViewById(R.id                        .assembly_searchact_showsearchitem_static);                convertView.setTag(_viewpage);            }            /*å¤„ç†æ–‡å­—ç­‰*/            _viewpage.title.setText(this.list.get(position));            _viewpage._static.setBackground(Tools.CreateDrawable(1, "#f30d88", "#ffffff", 5));            return convertView;        }        class viewpage {            TextView title;            TextView _static;        }    }}
+package shlm.lmcs.com.lazycat.LazyShopAct;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import shlm.lmcs.com.lazycat.LazyCatProgramUnt.CompanyAct.LazyCatAct;
+import shlm.lmcs.com.lazycat.LazyCatProgramUnt.Tools;
+import shlm.lmcs.com.lazycat.LazyCatProgramUnt.Views.AutoLineLayout;
+import shlm.lmcs.com.lazycat.R;
+
+/**
+ * ËÑË÷½çÃæ
+ */
+public class SearchAct extends LazyCatAct {
+    private AutoLineLayout everyoneSearch;
+    private AutoLineLayout searchLog;
+    private EditText input;
+    private ListView SearchlogListview;
+    private TextView btn_search;
+
+    @SuppressLint({"NewApi", "ResourceType"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_search);
+        setTransparentBar();
+        super.onCreate(savedInstanceState);
+        input = findViewById(R.id.assembly_head_input);
+        input.setBackground(Tools.CreateDrawable(3, getResources().getString(R.color.ThemeColor),
+                "#ffffff", 10));
+        btn_search = findViewById(R.id.assembly_head_searchTitle);
+        SearchlogListview = findViewById(R.id.activity_search_listview);
+        Tools.getFoucus(input);
+
+        init();
+    }
+
+    private void init() {
+
+        /**
+         * ±à¼­¿òµÄ¼àÌıÊÂ¼ş
+         */
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                /**
+                 * ¼ì²éÊÇ·ñÃ»ÓĞÊı¾İ Èç¹ûÃ»ÓĞ ¾Í²»Òª½øĞĞÁĞ±í²Ù×÷
+                 */
+                if (TextUtils.isEmpty(s.toString())) {
+                    /*Îª¿Õ*/
+                    findViewById(R.id.activity_search_listview).setVisibility(View.GONE);
+                    findViewById(R.id.activity_search_RefreshScrollView).setVisibility(View
+                            .VISIBLE);
+                } else {
+                    /*²»Îª¿Õ*/
+
+                    /*Ä£ÄâÍøÂç·ÃÎÊÓĞÊı¾İ*/
+                    ArrayList<String> text_list2 = new ArrayList<>();
+                    text_list2.add("Á÷ÀËµØÇò");
+                    text_list2.add("åĞÒ£É¢ÈË");
+                    text_list2.add("¶«¹¬");
+                    text_list2.add("EXO");
+                    text_list2.add("ÀÏ·¬ÇÑ");
+                    text_list2.add("¼ÙÃæÆïÊ¿ZI-O");
+                    text_list2.add("ĞÅÊÄµ©µ©");
+                    SearchLogAdapter adapter = new SearchLogAdapter(text_list2);
+                    SearchlogListview.setAdapter(adapter);
+                    findViewById(R.id.activity_search_listview).setVisibility(View.VISIBLE);
+                    findViewById(R.id.activity_search_RefreshScrollView).setVisibility(View.GONE);
+                }
+                //Ö´ĞĞ¶¯»­
+                if (TextUtils.isEmpty(s.toString())) {
+                    /*ÊäÈë¿òµÄÊı¾İÎª¿Õ*/
+                    btn_search.clearAnimation();
+                    /*Öğ½¥Òş²Ø¶¯»­*/
+                    btn_search.startAnimation(Tools.clearOnalpha(1000, false));
+                    btn_search.setVisibility(View.GONE);
+                    /*listView²»ÏÔÊ¾*/
+                } else if (btn_search.getVisibility() != View.VISIBLE) {
+                    //²»ÔÚÏÔÊ¾
+                    btn_search.clearAnimation();
+                    btn_search.setVisibility(View.VISIBLE);
+                    btn_search.clearAnimation();
+                    btn_search.startAnimation(Tools.createOnalpha(1000, false));
+                }
+            }
+        });
+
+
+        /**
+         * µã»÷ËÑË÷¿òµÄ¼àÌıÊÂ¼ş
+         */
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+        ArrayList<String> text_list = new ArrayList<>();
+        text_list.add("Á÷ÀËµØÇò");
+        text_list.add("åĞÒ£É¢ÈË");
+        text_list.add("¶«¹¬");
+        text_list.add("EXO");
+        text_list.add("ÀÏ·¬ÇÑ");
+        text_list.add("¼ÙÃæÆïÊ¿ZI-O");
+        text_list.add("ĞÅÊÄµ©µ©");
+        ArrayList<String> text_list2 = new ArrayList<>();
+        text_list2.add("Á÷ÀËµØÇò");
+        text_list2.add("åĞÒ£É¢ÈË");
+        text_list2.add("¶«¹¬");
+        text_list2.add("EXO");
+        text_list2.add("ÀÏ·¬ÇÑ");
+        text_list2.add("¼ÙÃæÆïÊ¿ZI-O");
+        text_list2.add("ĞÅÊÄµ©µ©");
+        ArrayList<LinearLayout> tv_list = Tools.handleToarraylist(getApplicationContext(),
+                text_list, 20, 10, 20, 10, "#e9e9e9", "#666666", 13);
+        ArrayList<LinearLayout> tv_list2 = Tools.handleToarraylist(getApplicationContext(),
+                text_list2, 20, 10, 20, 10, "#e9e9e9", "#666666", 13);
+
+        everyoneSearch = findViewById(R.id.activity_everyone_searchBody);/*´ó¼Ò¶¼ÔÚËÑ*/
+        searchLog = findViewById(R.id.activity_search_searchLogBody);/*ÓÃ»§µÄËÑË÷¼ÇÂ¼*/
+        for (int i = 0; i < tv_list.size(); i++) {
+            everyoneSearch.addView(tv_list.get(i));
+        }
+        for (int i = 0; i < tv_list2.size(); i++) {
+            searchLog.addView(tv_list2.get(i));
+        }
+    }
+
+    class SearchLogAdapter extends BaseAdapter {
+        private ArrayList<String> list;
+
+        public SearchLogAdapter(ArrayList<String> _list) {
+            this.list = _list;
+        }
+
+        @Override
+        public int getCount() {
+            return this.list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @SuppressLint("NewApi")
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //assembly_searchact_showsearchitem_title
+            viewpage _viewpage = null;
+            if (convertView != null) {
+                _viewpage = (viewpage) convertView.getTag();
+
+            } else {
+                convertView = LayoutInflater.from(getApplicationContext()).inflate(R.layout
+                        .assembly_searchact_showsearchitem, null);
+                _viewpage = new viewpage();
+                _viewpage.title = convertView.findViewById(R.id
+                        .assembly_searchact_showsearchitem_title);
+                _viewpage._static = convertView.findViewById(R.id
+                        .assembly_searchact_showsearchitem_static);
+                convertView.setTag(_viewpage);
+            }
+            /*´¦ÀíÎÄ×ÖµÈ*/
+            _viewpage.title.setText(this.list.get(position));
+            _viewpage._static.setBackground(Tools.CreateDrawable(1, "#f30d88", "#ffffff", 5));
+
+            return convertView;
+        }
+
+        class viewpage {
+            TextView title;
+            TextView _static;
+        }
+    }
+}
