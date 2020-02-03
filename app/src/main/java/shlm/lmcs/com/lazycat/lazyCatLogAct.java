3fa2da1a22fa08b5fd41c6ca851eb9089738a867
= <=>
2020-02-03 22:34:44 +0800

    update

--- app/src/main/java/shlm/lmcs/com/lazycat/lazyCatLogAct.java
+++ app/src/main/java/shlm/lmcs/com/lazycat/lazyCatLogAct.java
@@ -33,7 +33,6 @@ import shlm.lmcs.com.lazycat.LazyShopAct.PromotionAct;
 import shlm.lmcs.com.lazycat.LazyShopAct.SystemAct.SystemGuidepage;
 import shlm.lmcs.com.lazycat.LazyShopTools.LocalProgramTools;
 import shlm.lmcs.com.lazycat.TerminalSystemMO.Record.SystemVs;
-import shlm.lmcs.com.lazycat.TerminalSystemOS.OS;
 
 import static shlm.lmcs.com.lazycat.LazyCatProgramUnt.Tools.isPermission;
 
@@ -76,7 +75,6 @@ public class lazyCatLogAct extends LazyCatAct {
                     }
                 } else {
                     LazyCatActStartActivity(MainAct.class, true);
-
                 }
                 super.handleMessage(msg);
             }
@@ -87,6 +85,9 @@ public class lazyCatLogAct extends LazyCatAct {
          * 测试代码区
          */
 
+
+
+/*
         OS._saveOrder(lazyCatLogAct.this, "1", "onlyid", "122", new OS.onSaveorder() {
             @Override
             public void onSaveOk() {
@@ -103,6 +104,7 @@ public class lazyCatLogAct extends LazyCatAct {
 
             }
         });
+*/
         /*保存系统的访问记录*/
         SystemVs._start(lazyCatLogAct.this);
         /*找到Ico控件*/
